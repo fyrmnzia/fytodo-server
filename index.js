ace9682f183532a11e709df4cc4b385e6189cdb5
fyrmnzia <1rifqi.firmansyah@gmail.com>
2025-01-30 05:06:08 +0700

    debug

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.get("/", async (req, res) => {
   try {
     const result = await db`SELECT version()`;
-    res.json({ db_version: result[0].version });
+    res.json({ db_version: result[0].version, msg: "Home Page" });
   } catch (error) {
     res.status(500).json({
       error: "Gagal mengambil versi database",
